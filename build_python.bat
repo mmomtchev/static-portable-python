2e804ff6f6c913bced9589a999883512b4d97287
Momtchil Momtchev <momtchil@motmchev.com>
2024-05-27 16:49:45 +0200

    fix Windows

--- build_python.bat
+++ build_python.bat
@@ -1,7 +1,12 @@
 set mypath=%~dp0
 cd %mypath:~0,-1%
 
+set PYTHONHOME=
+set PYTHONPATH=
+
 if not exist %PYTHON_DIST% mkdir %PYTHON_DIST%
+if not exist %PYTHON_BUILD% mkdir %PYTHON_BUILD%
+if not exist %1 mkdir %1
 if not exist %PYTHON_DIST%\Python-%PYTHON_VERSION%.tgz (
   curl https://www.python.org/ftp/python/%PYTHON_VERSION%/Python-%PYTHON_VERSION%.tgz --output %PYTHON_DIST%\Python-%PYTHON_VERSION%.tgz
 )
@@ -11,8 +16,8 @@ if not exist "%1\python312.lib" (
   rd /q /s %PYTHON_BUILD%\Python-%PYTHON_VERSION%
 
   tar -C %PYTHON_BUILD% -zxf %PYTHON_DIST%\Python-%PYTHON_VERSION%.tgz
-  %PYTHON_BUILD%\Python-%PYTHON_VERSION%\PCBuild\%PYTHON_BUILD%.bat
-  mkdir -p %1\DLLs
+  %PYTHON_BUILD%\Python-%PYTHON_VERSION%\PCBuild\build.bat
+  if not exist %1\DLLs mkdir %1\DLLs
   (robocopy %PYTHON_BUILD%\Python-%PYTHON_VERSION%\PCBuild\amd64 %1\DLLs /MIR) ^& if %ERRORLEVEL% leq 1 set ERRORLEVEL = 0
   (robocopy %PYTHON_BUILD%\Python-%PYTHON_VERSION%\Lib %1\lib /MIR) ^& if %ERRORLEVEL% leq 1 set ERRORLEVEL = 0
   (robocopy %PYTHON_BUILD%\Python-%PYTHON_VERSION%\Include %1\include /MIR) ^& if %ERRORLEVEL% leq 1 set ERRORLEVEL = 0
