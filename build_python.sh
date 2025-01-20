b4376d8ad6484834ffa0776713e6691baaded1f6
Momtchil Momtchev <momtchil@momtchev.com>
2025-01-20 17:00:37 +0100

    add the pkg-config files

--- build_python.sh
+++ build_python.sh
@@ -31,7 +31,7 @@ if [ ! -d "$1" ] || [ ! -r "${LIBNAME}" ]; then
   source conan/conanbuild.sh
 
   echo ${SEP}
-  PKGS="zlib bzip2 liblzma libgettext openssl"
+  PKGS="zlib bzip2 liblzma libgettext openssl ncurses uuid"
   echo "conan CFLAGS=${CFLAGS}"
   echo "conan LDFLAGS=${LDFLAGS}"
   echo ${SEP}
