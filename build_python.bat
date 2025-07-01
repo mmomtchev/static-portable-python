b6c540412d2b0a50163beb8c20ec2218db29f4a0
Momtchil <momtchil@momtchev.com>
2025-07-01 19:27:54 +0100

    support building on Windows ARM64

--- build_python.bat
+++ build_python.bat
@@ -11,14 +11,25 @@ if not exist %PYTHON_DIST%\Python-%PYTHON_VERSION%.tgz (
   curl https://www.python.org/ftp/python/%PYTHON_VERSION%/Python-%PYTHON_VERSION%.tgz --output %PYTHON_DIST%\Python-%PYTHON_VERSION%.tgz
 )
 
+if %PROCESSOR_ARCHITECTURE%==AMD64 (
+  set ARCH=x64
+  set ARCH_DIR=amd64
+) else if %PROCESSOR_ARCHITECTURE%==ARM64 (
+  set ARCH=ARM64
+  set ARCH_DIR=arm64
+) else (
+  echo %PROCESSOR_ARCHITECTURE% not supported, only AMD64 and ARM64
+  exit 1
+)
+
 if not exist "%1\python3*.lib" (
   echo building in %1
   rd /q /s %PYTHON_BUILD%\Python-%PYTHON_VERSION%
 
   tar -C %PYTHON_BUILD% -zxf %PYTHON_DIST%\Python-%PYTHON_VERSION%.tgz
-  %PYTHON_BUILD%\Python-%PYTHON_VERSION%\PCBuild\build.bat
+  %PYTHON_BUILD%\Python-%PYTHON_VERSION%\PCBuild\build.bat -p %ARCH%
   if not exist %1\DLLs mkdir %1\DLLs
-  (robocopy %PYTHON_BUILD%\Python-%PYTHON_VERSION%\PCBuild\amd64 %1\DLLs /MIR) ^& if %ERRORLEVEL% leq 1 set ERRORLEVEL = 0
+  (robocopy %PYTHON_BUILD%\Python-%PYTHON_VERSION%\PCBuild\%ARCH_DIR% %1\DLLs /MIR) ^& if %ERRORLEVEL% leq 1 set ERRORLEVEL = 0
   (robocopy %PYTHON_BUILD%\Python-%PYTHON_VERSION%\Lib %1\lib /MIR) ^& if %ERRORLEVEL% leq 1 set ERRORLEVEL = 0
   (robocopy %PYTHON_BUILD%\Python-%PYTHON_VERSION%\Include %1\include /MIR) ^& if %ERRORLEVEL% leq 1 set ERRORLEVEL = 0
   copy %PYTHON_BUILD%\Python-%PYTHON_VERSION%\PC\pyconfig.h %1\include
