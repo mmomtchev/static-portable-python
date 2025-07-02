5362add7b425a3781a0dce047a0c3301364cc1b4
Momtchil <momtchil@momtchev.com>
2025-07-02 21:39:33 +0100

    fix the message

--- build_python.bat
+++ build_python.bat
@@ -11,12 +11,13 @@ if not exist %PYTHON_DIST%\Python-%PYTHON_VERSION%.tgz (
   curl https://www.python.org/ftp/python/%PYTHON_VERSION%/Python-%PYTHON_VERSION%.tgz --output %PYTHON_DIST%\Python-%PYTHON_VERSION%.tgz
 )
 
-echo "Windows CPU Architecture : %PROCESSOR_ARCHITECTURE% / %PROCESSOR_ARCHITEW6432%"
+echo Windows CPU Architecture : %PROCESSOR_ARCHITECTURE%
 
 if "%PROCESSOR_ARCHITECTURE%"=="AMD64" (
   set ARCH=x64
   set ARCH_DIR=amd64
 ) else if "%PROCESSOR_ARCHITEW6432%"=="AMD64" (
+  echo Running on %PROCESSOR_ARCHITEW6432%
   set ARCH=x64
   set ARCH_DIR=amd64
 ) else if "%PROCESSOR_ARCHITECTURE%"=="ARM64" (
